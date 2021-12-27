de37e825b563be06e0e9b9f00523442ef6027f93
Martin Čech <2martinho2@gmail.com>
2021-12-27 14:59:34 +0100

    přidání slideru na stránku

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,52 @@
-var button = document.querySelector('#button');
-var modal = document.querySelector('#modal');
-var close = document.querySelector('#close');
+//var button = document.querySelector('#button');
+//var modal = document.querySelector('#modal');
+//var close = document.querySelector('#close');
 
-button.addEventListener('click', function(){
-    modal.classList.add('modal_active');
+//button.addEventListener('click', function(){
+//    modal.classList.add('modal_active');
 
-});
+//});
 
-close.addEventListener('click', function(){
-    modal.classList.remove('modal_active');
+//close.addEventListener('click', function(){
+//    modal.classList.remove('modal_active');
+
+//});
+
+
+
+$(document).ready(function(){
+    var button = $('#button');
+    var modal = $('#modal');
+    var close = $('#close');
+
+    button.on('click', function(){
+        modal.addClass('modal_active');
+    });
+
+    close.on('click', function(){
+        modal.removeClass('modal_active');
+    });
+
+    $('.slider').slick({
+        slidesToShow: 3,
+        prevArrow: $('.arrows__left'),
+        nextArrow: $('.arrows__right'),
+        responsive: [
+            {
+              breakpoint: 1200,
+              settings: {
+                slidesToShow: 2,
+               
+              }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+          
+          ]
+    });
 
 });
